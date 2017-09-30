00561cb4434f8fcaf44e998251f662a8af85145f
Jacob van't Hoog <vantjac@gmail.com>
2017-09-30 09:52:43 -0700

    Interpret basic turtle commands

--- logo.js
+++ logo.js
@@ -4,6 +4,8 @@ VOCAB = VOCAB.split(" ");
 WIDTH = 512;
 HEIGHT = 512;
 
+GENERIC_COMMAND_ERROR = "Can't interpret command";
+
 var ctx = document.getElementById("drawcanvas").getContext("2d");
 var spriteCtx = document.getElementById("spritecanvas").getContext("2d");
 
@@ -16,7 +18,72 @@ var turtle = {
 }
 
 function runCommand(tokens) {
-    console.log(tokens);
+    if(tokens[0] == "move") {
+        if(tokens.length == 1)
+            return GENERIC_COMMAND_ERROR;
+        return moveCommand(tokens.slice(1));
+    }
+    if(tokens[0] == "forward" || tokens[0] == "backward") {
+        return moveCommand(tokens);
+    }
+    if(tokens[0] == "turn") {
+        if(tokens.length == 1)
+            return GENERIC_COMMAND_ERROR;
+        return turnCommand(tokens.slice(1));
+    }
+    if(tokens[0] == "left" || tokens[0] == "right") {
+        return turnCommand(tokens);
+    }
+    if(tokens[0] == "pen") {
+        if(tokens.length == 1)
+            return GENERIC_COMMAND_ERROR;
+        return penCommand(tokens.slice(1));
+    }
+    if(tokens[0] == "up" || tokens[0] == "down" || tokens[0] == "color") {
+        return penCommand(tokens.slice(0));
+    }
+    return GENERIC_COMMAND_ERROR;
+}
+
+function moveCommand(tokens) {
+    if(tokens[0] != "forward" && tokens[0] != "backward")
+        return GENERIC_COMMAND_ERROR;
+    if(tokens.length == 1)
+        return GENERIC_COMMAND_ERROR;
+    var steps = tokens[1];
+    if(isNaN(steps))
+        return steps + " is not a number";
+    steps = Number(steps);
+    if(tokens[0] == "backward")
+        steps = -steps;
+    turtleMove(steps);
+}
+
+function turnCommand(tokens) {
+    if(tokens[0] != "left" && tokens[0] != "right")
+        return GENERIC_COMMAND_ERROR;
+    if(tokens.length == 1)
+        return GENERIC_COMMAND_ERROR;
+    var deg = tokens[1];
+    if(isNaN(deg))
+        return deg + " is not a number";
+    deg = Number(deg);
+    if(tokens[0] == "left")
+        deg = -deg;
+    turtleTurn(deg);
+}
+
+function penCommand(tokens) {
+    if(tokens[0] == "up")
+        turtle.pen = false;
+    else if(tokens[0] == "down")
+        turtle.pen = true;
+    else if(tokens[0] == "color") {
+        if(tokens.length == 1)
+            return GENERIC_COMMAND_ERROR;
+        turtle.color = tokens[1];
+    } else
+        return GENERIC_COMMAND_ERROR;
 }
 
 function updateSprite() {
