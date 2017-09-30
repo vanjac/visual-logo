0f8ff6658e8dc1ee3444ec81f513f9085ef99024
Jacob van't Hoog <vantjac@gmail.com>
2017-09-29 23:19:30 -0700

    More blocks

--- blocks.js
+++ blocks.js
@@ -11,17 +11,37 @@ BLOCKS = [
         }
     },
     {
-        "text": "forward ___",
+        "text": "forward ___ steps",
         "rule": function(tokens) {
             return contains(tokens, "move") &&
                 !(contains(tokens, "forward") || contains(tokens, "backward"));
         }
     },
     {
-        "text": "backward ___",
+        "text": "backward ___ steps",
         "rule": function(tokens) {
             return contains(tokens, "move") &&
                 !(contains(tokens, "forward") || contains(tokens, "backward"));
+        }
+    },
+    {
+        "text": "turn",
+        "rule": function(tokens) {
+            return tokens.length == 0;
+        }
+    },
+    {
+        "text": "left ___ degrees",
+        "rule": function(tokens) {
+            return contains(tokens, "turn") &&
+                !(contains(tokens, "left") || contains(tokens, "right"));
+        }
+    },
+    {
+        "text": "right ___ degrees",
+        "rule": function(tokens) {
+            return contains(tokens, "turn") &&
+                !(contains(tokens, "left") || contains(tokens, "right"));
         }
     }
 
